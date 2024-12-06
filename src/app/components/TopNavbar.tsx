317c2e43fa43381a5b818cdb35493cdbff8b3f13
abdulApp <abdulrahman_alamoudi@hotmail.com>
2024-12-06 04:13:21 +0300

    first commit

---
+++ src/app/components/TopNavbar.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import Image from "next/image";
+import React, { useState } from "react";
+import Logo from "@/app/assets/Icon.svg";
+
+export default function TopNavbarComponent() {
+    const [btn, setBtn] = useState("")
+  return (
+    <div className="fixed top-10 w-full h-16 px-10">
+      <div className="h-full w-full flex justify-between items-center">
+        <div className="flex items-center gap-4">
+          <Image src={Logo} alt="Positivus Logo" width={25} height={25} />
+          <h1 className="text-3xl font-bold tracking-widest">Positivus</h1>
+        </div>
+
+        <nav className="text-xl">
+          <ul className="flex items-center gap-6">
+            <li className="cursor-pointer px-4 py-2 rounded-full hover:bg-[#B9FF66]">About Us</li>
+            <li className="cursor-pointer px-4 py-2 rounded-full hover:bg-[#B9FF66]">Services</li>
+            <li className="cursor-pointer px-4 py-2 rounded-full hover:bg-[#B9FF66]">Use Cases</li>
+            <li className="cursor-pointer px-4 py-2 rounded-full hover:bg-[#B9FF66]">Pricing</li>
+            <li className="cursor-pointer px-4 py-2 rounded-full hover:bg-[#B9FF66]">Blog</li>
+          </ul>
+        </nav>
+
+        <button
+          className={`bg-white text-black border border-r-4 border-b-4 border-black py-4 px-8 rounded-xl shadow-xl 
+            `}
+            // ${btn}
+          type="button"
+        //   onClick={() => setBtn("border-b-2 border-r-2")}
+        >
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import TopNavbar from "./components/TopNavbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <TopNavbar />
         {children}
       </body>
     </html>
