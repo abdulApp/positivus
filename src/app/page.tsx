ff58bd230b0afb8cc976570a79bd5ca35bbfc047
abdulApp <abdulrahman_alamoudi@hotmail.com>
2024-12-06 21:27:47 +0300

    finish learn more section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
-import Hero from "../section/Hero";
-import Services from "../section/Services";
-import LearnMore from "@/section/LearnMore";
+import Hero from "./section/Hero";
+import Services from "./section/Services";
+import LearnMore from "@/app/section/LearnMore";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <Services />
-      {/* <LearnMore /> */}
+      <LearnMore />
     </div>
   );
 }
---
+++ src/app/section/LearnMore.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Image from "next/image";
+import LearnMoreImg from "@/app/assets/LearnMoreImg.png";
+
+export default function LearnMore() {
+  return (
+    <div className="mt-16 w-screen px-16">
+      <div className="w-full h-[500px] flex justify-around items-center bg-[#F3F3F3] rounded-3xl">
+        <div className="h-full flex flex-col justify-center items-start gap-12">
+          <h1 className="text-5xl">Let’s make things happen</h1>
+          <p className="text-xl">
+            Contact us today to learn more about how our digital<br/>marketing
+            services can help your business grow and<br/>succeed online.
+          </p>
+          <button className="border border-black bg-black text-2xl text-white px-10 py-6 rounded-2xl">Get your free proposal</button>
+        </div>
+        <div className="flex flex-col justify-center gap-4">
+          <Image src={LearnMoreImg} alt="Learn More" width={500} height={500} />
+        </div>
+      </div>
+    </div>
+  );
+}
